2189913cbf350ef54f0e043f81b3a5274b9229c1
Phil Tyler <laidbackcoder@gmail.com>
2025-01-08 22:19:47 +0000

    Initial Load

---
+++ README.md
@@ -0,0 +1,16 @@
+# 🔐 Password Generator
+
+Generate strong Pseudorandom Passwords for better Security
+
+Learn more: https://en.wikipedia.org/wiki/Pseudorandom_number_generator
+
+## Getting Started
+
+Install the dependencies and run the project
+
+```
+npm install
+npm start
+```
+
+Head over to https://vitejs.dev/ to learn more about configuring vite
---
+++ src/index.js
@@ -0,0 +1,44 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+let numChars = 50;
+let includeSymbols = true;
+
+document.getElementById("password-length-em").value = numChars;
+document.getElementById("include-symbols-em").checked = includeSymbols;
+
+function updateLength() {
+    numChars = document.getElementById("password-length-em").value;
+}
+
+function updateIncludeSymbols() {
+    includeSymbols = document.getElementById("include-symbols-em").checked;
+}
+
+function generatePassword() {
+    let maxCharacterPosition = includeSymbols ? characters.length : characters.length - 30;
+    let password = "";
+
+    for (let i=0; i < numChars; i++){
+        password += characters[Math.floor(Math.random() * maxCharacterPosition)];
+    }
+
+    document.getElementById("password-em").textContent = password;
+}
+
+function copyPasswordToClipboard() {
+    let password = document.getElementById("password-em").textContent;
+
+    if (password != "") {
+        navigator.clipboard.writeText(password);
+        document.getElementById("notification").style.display = "block";
+
+        setTimeout(() => {
+            document.getElementById("notification").style.animation = "notification-out 1s ease";
+            setTimeout(() => {
+                document.getElementById("notification").style.display = "none";
+                document.getElementById("notification").style.animation = "notification-in 1s ease";
+            }, 1000)
+        }, 4000)
+    }
+}
