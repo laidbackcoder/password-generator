16f71799c394f594066b7efa4739fe2b2ce5cf6e
Phil Tyler <laidbackcoder@gmail.com>
2025-02-03 15:45:53 +0000

    Added Basic SSL library for testing

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
 import {defineConfig} from "vite"
+import basicSsl from "@vitejs/plugin-basic-ssl"
 
 
 export default defineConfig({
-	root: "src"
+	root: "src",
+	plugins: [basicSsl()],
+	server: {
+		https: true, // same as "--https" flag
+		host: true, // same as "--host" flag
+	},
 })
